a6836023e48f398b034c9332aa8ab8d137f07bf3
doszp <147741629+doszp@users.noreply.github.com>
2025-11-23 22:16:57 -0300

    Adicionado modelos de Instituição, curso e disciplina

--- projectuniversitysystem/universitysystem/models.py
+++ projectuniversitysystem/universitysystem/models.py
@@ -8,3 +8,24 @@ except Exception:
     sys.exit()
 
 # Create your models here.
+class Disciplina(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Nome = models.CharField(max_length=256)
+    Codigo = models.CharField(max_length=16)
+    Descricao = models.TextField()                   
+    Curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+class Curso(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Nome = models.CharField(max_length=256)
+    Descricao = models.TextField()           
+    Sigla = models.CharField(max_length=16)
+    Horarios = models.CharField(max_length=32)
+    Instituicao = models.ForeignKey(Instituicao, on_delete=models.PROTECT)
+
+class Instituicao(models.Model):
+    ID = models.AutoField(primary_key=True)
+    Nome = models.CharField(max_length=256)
+    Sigla = models.CharField(max_length=16)
+    CNPJ = models.CharField(max_length=32)
+    Endereco = models.CharField(max_length=512)    
