e94ed7732b1057f40cebbb33492f591d6d61668c
rodrigomotta01 <rodrigomotta@ufrrj.br>
2025-12-12 16:52:23 -0300

    Profile finalizado

--- projectuniversitysystem/universitysystem/views.py
+++ projectuniversitysystem/universitysystem/views.py
@@ -213,8 +213,6 @@ def profile_view(request):
 
     return render(request, 'gerais/profile.html', context)
 
-
-
 def delete_foto_view(request):
     if not request.user.is_authenticated:
         return redirect("login")
@@ -230,6 +228,22 @@ def delete_foto_view(request):
         
     return redirect('profile')
 
+def change_foto_view(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+    
+    if request.method == "POST":
+    
+        user = request.user
+        
+        nova_foto = request.FILES.get('foto')
+        if nova_foto:
+            if user.foto:
+                user.foto.delete()
+            user.foto = nova_foto
+            user.save()
+        
+    return redirect('profile')
 
 def historico_view(request):
     if not request.user.is_authenticated:
